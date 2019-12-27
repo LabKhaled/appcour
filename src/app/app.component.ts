afa78fa8886ab78df3a58b881fb44190fabe91a7
khaled <etudiant.chalouati.khaled@uvt.tn>
2019-12-27 22:55:21 +0100

    intiale commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component,Input,OnInit } from '@angular/core';
+import {AppareilService} from './Services/appareil.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appcour';
+export class AppComponent  implements OnInit{
+  isAuth= true;
+  appareils:any[];
+
+
+  constructor(private appareilServ: AppareilService)
+  {    
+        setTimeout(
+                     ()=>{this.isAuth=false;},4000);
+
+  }
+
+  ngOnInit(){
+
+    this.appareils=this.appareilServ.appareils;
+
+
+  }
+  Touteteindre(){
+    this.appareilServ.switchOffAll();
+    
+  }
+
+  Toutallumer(){
+
+    this.appareilServ.switchOnAll();
+     
+   } 
+
+ 
 }
